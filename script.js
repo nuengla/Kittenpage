619e46dd25c9e312c02e26be65ebfa65fd96341d
nuengla <81779600+nuengla@users.noreply.github.com>
2021-10-25 09:54:05 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+var nav = document.querySelector(".nav");
+var menu = document.querySelector(".menu");
+var close = document.querySelector("#close");
+var body = document.querySelector("body");
+nav.style.display = "none";
+function shownav(){
+    nav.style.display = "block";
+    menu.style.display = "none";
+    close.style.display = "block";
+    body.style.overflowY = "hidden";
+    
+}
+function hidenav(){
+    nav.style.display = "none";
+    menu.style.display = "block";
+    close.style.display = "none";
+    body.style.overflowY = "scroll";
+}
